a74b0dbcae1cc72da5f87f7526954c7121e9f3f4
Juan Ignacio Giusiano <juani@MacBook-Air-de-Juan.local>
2025-04-14 19:23:03 -0300

    Se definen mejor las constantes

--- tarea-1/main.go
+++ tarea-1/main.go
@@ -2,18 +2,27 @@ package main
 
 import "fmt"
 
+const (
+	MAX_LIMIT      int = 10 // Constante para el número máximo de puntuaciones
+	MIN_SCORE      int = 1  // Constante para la puntuación mínima
+	MAX_SCORE      int = 5  // Constante para la puntuación máxima
+	MIN_GOOD_SCORE int = 4  // Constante para la puntuación mínima de buena puntuación
+	MIN_BAD_SCORE  int = 1  // Constante para la puntuación mínima de mala puntuación
+	MAX_GOOD_SCORE int = 5  // Constante para la puntuación máxima de buena puntuación
+	MAX_BAD_SCORE  int = 2  // Constante para la puntuación máxima de mala puntuación
+)
+
 func main() {
 	var score int
 	var numberBadScores int
 	var numberGoodScores int
-	const MAX_LIMIT int = 10
 	var scores = map[int]int{}
 
 	for i := 0; i < MAX_LIMIT; i++ {
 		fmt.Println("Ingrese un puntaje del 1 al 5: ")
 		fmt.Scan(&score)
 
-		if score < 1 || score > 5 {
+		if score < MIN_SCORE || score > MAX_SCORE {
 			fmt.Println("Puntaje inválido. Debe estar entre 1 y 5.")
 			i--
 		}
@@ -38,17 +47,17 @@ func countAndAssignScore(score int, scores map[int]int, numberGoodScores *int, n
 	if !existsKey {
 		scores[score] = 1
 
-		if score == 4 || score == 5 {
+		if score == MIN_GOOD_SCORE || score == MAX_GOOD_SCORE {
 			*numberGoodScores = 1
-		} else if score == 1 || score == 2 {
+		} else if score == MIN_BAD_SCORE || score == MAX_BAD_SCORE {
 			*numberBadScores = 1
 		}
 	} else {
 		scores[score]++
 
-		if score == 4 || score == 5 {
+		if score == MIN_GOOD_SCORE || score == MAX_GOOD_SCORE {
 			*numberGoodScores++
-		} else if score == 1 || score == 2 {
+		} else if score == MIN_BAD_SCORE || score == MAX_BAD_SCORE {
 			*numberBadScores++
 		}
 	}
